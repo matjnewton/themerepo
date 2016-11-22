8a31e8303da341c4c942d9b91cf9f752bd006677
dgamoni <dgamoni@gmail.com>
2016-11-22 14:01:27 +0200

    add product plugins

--- includes/load.php
+++ includes/load.php
@@ -5,7 +5,15 @@ define('CORE_URL', get_stylesheet_directory_uri()  . '/includes');
 define( 'CORE_PLUGINS_PATH', CORE_PATH . '/plugins/' );
 define( 'CORE_PLUGINS_URL', CORE_URL . '/plugins/' );
 
-// autoload functions
+/* load plugins */
+
+// acf- font-awesome icon
+require_once CORE_PLUGINS_PATH.'/advanced-custom-fields-font-awesome/acf-font-awesome.php';
+// add plugins bfi tumb
+require_once CORE_PLUGINS_PATH.'/BFI_Thumb.php';
+
+/* autoload functions */
+
 $dirs = array(
     CORE_PATH . '/post_types/',
     CORE_PATH . '/functions/',
