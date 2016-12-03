4306564bc27f0c60b8cae436e1601cbb9013006e
dgamoni <dgamoni@gmail.com>
2016-12-03 08:01:52 +0200

    add tiger live functions

--- includes/functions/rezdy_functions.php
+++ includes/functions/rezdy_functions.php
@@ -1,90 +1,92 @@
 <?php
 
-function get_rezdy_tour_select($field) {
-	$field['choices'] = array();
-    $field['choices']['0'] = 'no sync';
+/* migrate to tiger_live.php */
+
+// function get_rezdy_tour_select($field) {
+// 	$field['choices'] = array();
+//     $field['choices']['0'] = 'no sync';
     
-	global $wpdb;
-    $table = $wpdb->prefix."options";
-    $query = "SELECT * FROM ".$table." WHERE `option_name` =  'options_check_user_id'";
-    $results = $wpdb->get_row($query);
-    $api_key = $results->option_value;
+// 	global $wpdb;
+//     $table = $wpdb->prefix."options";
+//     $query = "SELECT * FROM ".$table." WHERE `option_name` =  'options_check_user_id'";
+//     $results = $wpdb->get_row($query);
+//     $api_key = $results->option_value;
 
-    //$api_key = get_field('field_n1993k2903', 'option');
-    $product_url = "https://api.rezdy.com/v1/categories/924/products?limit=100&apiKey=".$api_key;
+//     //$api_key = get_field('field_n1993k2903', 'option');
+//     $product_url = "https://api.rezdy.com/v1/categories/924/products?limit=100&apiKey=".$api_key;
 
-    $json = file_get_contents($product_url);
-    $rezdy = json_decode($json);
-    $rezdy_tour_name = array();
-    foreach($rezdy->products as $key=>$product){
-    	$rezdy_tour_name[] = $product->name;
-    	$field['choices'][ $product->productCode ] = $product->name;
-    }
+//     $json = file_get_contents($product_url);
+//     $rezdy = json_decode($json);
+//     $rezdy_tour_name = array();
+//     foreach($rezdy->products as $key=>$product){
+//     	$rezdy_tour_name[] = $product->name;
+//     	$field['choices'][ $product->productCode ] = $product->name;
+//     }
 
-    return $field;
+//     return $field;
 
-} 
+// } 
 
-add_filter('acf/load_field/name=our_tours_rezdy', 'get_rezdy_tour_select');
-
-
-function get_cpt_tours_select($field) {
-	$field['choices'] = array();
-
-	global $wpdb;
-	$table = $wpdb->prefix."posts";
-	$query = "SELECT * FROM ".$table." WHERE post_type='tour' AND post_status='publish'";
-	$results = $wpdb->get_results($query);
-	$tours_array = array();
-
-	foreach ($results as $res){
-	 	$tours_array[] = $res->post_title;
-	 	$field['choices'][ $res->ID ] = $res->post_title;
-	 }
-	 return $field;
-} 
-add_filter('acf/load_field/name=our_tours', 'get_cpt_tours_select');
+// add_filter('acf/load_field/name=our_tours_rezdy', 'get_rezdy_tour_select');
 
 
-function get_cpt_tours_select_checker($field) {
-	$field['choices'] = array();
+// function get_cpt_tours_select($field) {
+// 	$field['choices'] = array();
 
-	$tours_array = array();
-	$our_tours = array(); 
+// 	global $wpdb;
+// 	$table = $wpdb->prefix."posts";
+// 	$query = "SELECT * FROM ".$table." WHERE post_type='tour' AND post_status='publish'";
+// 	$results = $wpdb->get_results($query);
+// 	$tours_array = array();
 
-	if( have_rows('matching_products', 'option') ): 
-		while( have_rows('matching_products', 'option') ): the_row(); 
-			$our_tours[] = get_sub_field('our_tours');
-		endwhile; 
-	endif; 
+// 	foreach ($results as $res){
+// 	 	$tours_array[] = $res->post_title;
+// 	 	$field['choices'][ $res->ID ] = $res->post_title;
+// 	 }
+// 	 return $field;
+// } 
+// add_filter('acf/load_field/name=our_tours', 'get_cpt_tours_select');
 
 
-	foreach ($our_tours as $res){
-	 	$field['choices'][ $res ] = get_the_title( $res );
-	 }
-	 return $field;
-} 
-add_filter('acf/load_field/name=our_tours_checker', 'get_cpt_tours_select_checker');
+// function get_cpt_tours_select_checker($field) {
+// 	$field['choices'] = array();
 
-function save_rezdy_sync() {
-	$screen = get_current_screen();
-	if (strpos($screen->id, "acf-options-company-details") == true) {
-		//print_r($_POST['acf']['field_57eb6b8fd8aae'] );
-		//die();
-		$matching_products = $_POST['acf']['field_57eb6b8fd8aae'];
-		if ($matching_products) {
-				foreach ($matching_products as $key => $matching_product) {
-					// print_r($matching_product['field_57eb6b9ad8aaf']);
-					// die();
-					$postid = $matching_product['field_57eb6b9ad8aaf'];
-					$productcode = $matching_product['field_57eb6b9ad8aaf_rezdy'];
-					update_field('productcode', $productcode, $postid);
-				}
-		}
+// 	$tours_array = array();
+// 	$our_tours = array(); 
 
-	} 
-}
-add_action('acf/save_post', 'save_rezdy_sync', 20);
+// 	if( have_rows('matching_products', 'option') ): 
+// 		while( have_rows('matching_products', 'option') ): the_row(); 
+// 			$our_tours[] = get_sub_field('our_tours');
+// 		endwhile; 
+// 	endif; 
+
+
+// 	foreach ($our_tours as $res){
+// 	 	$field['choices'][ $res ] = get_the_title( $res );
+// 	 }
+// 	 return $field;
+// } 
+// add_filter('acf/load_field/name=our_tours_checker', 'get_cpt_tours_select_checker');
+
+// function save_rezdy_sync() {
+// 	$screen = get_current_screen();
+// 	if (strpos($screen->id, "acf-options-company-details") == true) {
+// 		//print_r($_POST['acf']['field_57eb6b8fd8aae'] );
+// 		//die();
+// 		$matching_products = $_POST['acf']['field_57eb6b8fd8aae'];
+// 		if ($matching_products) {
+// 				foreach ($matching_products as $key => $matching_product) {
+// 					// print_r($matching_product['field_57eb6b9ad8aaf']);
+// 					// die();
+// 					$postid = $matching_product['field_57eb6b9ad8aaf'];
+// 					$productcode = $matching_product['field_57eb6b9ad8aaf_rezdy'];
+// 					update_field('productcode', $productcode, $postid);
+// 				}
+// 		}
+
+// 	} 
+// }
+// add_action('acf/save_post', 'save_rezdy_sync', 20);
 
 function tour_output($product_code, $seatsavailable, $category_array, $product_id ) {
 
